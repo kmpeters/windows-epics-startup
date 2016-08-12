1f534e0221c4a6c52066a2427723fe584adddef5
Kevin Peterson <kmpeters@anl.gov>
2016-08-12 15:50:43 +0000

    Initial import of EPICS startup scripts for Windows
    git-svn-id: https://subversion.xray.aps.anl.gov/kmp/windows_epics_startup/trunk@797 f095f811-4e88-6c84-d33e-e382bdb4531c

---
+++ VS15/win32-x86-debug.bat
@@ -0,0 +1,37 @@
+@ECHO OFF
+
+REM   --- Convenience ---
+REM set BASE=base-3.14.12.5
+
+REM   --------------- Windows binaries -------------------
+set PATH=C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\SYSTEM32\Wbem
+
+REM   --------------- Strawberry perl -------------------
+set PATH=C:\Strawberry\perl\bin;%PATH%
+
+REM   --------------- re2c & gnuwin32 make ----------------------
+set PATH=D:\bin;%PATH%
+
+REM    ----- Visual Studio 2010 -----
+REM --  win32-x86 ---
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+
+REM --------------- EPICS --------------------------------
+set EPICS_HOST_ARCH=win32-x86-debug
+REM set PATH=%PATH%;D:\epics\%BASE%\bin\%EPICS_HOST_ARCH%
+REM set PATH=%PATH%;D:\epics\extensions\bin\%EPICS_HOST_ARCH%
+
+REM ----------------- GNU make flags ---------------------
+REM set MAKEFLAGS=-w
+
+REM --------------- Epics Channel Access -----------------
+REM set EPICS_CA_ADDR_LIST=n.n.n.n  n.n.n.n
+REM set EPICS_CA_AUTO_ADDR_LIST=YES
+
+REM ======================================================
+REM --------------- Exceed -------------------------------
+REM    Needed for X11 extensions
+REM ======================================================
+REM set EX_VER=14.00
+REM set PATH=%PATH%;C:\Exceed%EX_VER%\XDK\
+REM set PATH=%PATH%;C:\Program Files\Hummingbird\Connectivity\%EX_VER%\Exceed\
---
+++ epics.bat
@@ -0,0 +1,83 @@
+@ECHO OFF
+
+setlocal
+
+:GET_VS_VER
+echo Versions of Visual Studio available: 10, 15
+set /p vs=Enter Visual Studio Version: 
+REM echo %vs%
+if defined vs (
+  if "%vs%" == "10" set verIsValid=1
+  if "%vs%" == "15" set verIsValid=1
+) else (
+  echo   Using default: Visual Studio 10
+  set vs=10
+  goto GET_ARCH
+)
+
+if defined verIsValid (
+  echo    You chose %vs%
+) else (
+  echo    Invalid version!
+  goto GET_VS_VER
+)
+
+:GET_ARCH
+echo Valid EPICS_HOST_ARCH suffixes:
+echo 32             64
+echo 32-debug       64-debug
+echo 32-static      64-static
+set /p eha=Enter suffix: 
+REM eha gets set to the first compared item, if it is blank
+if "%eha%" == "32" (
+  set archIsValid=1
+  set archIs64bit=0
+) else if "%eha%" == "32-debug" (
+  set archIsValid=1
+  set archIs64bit=0
+) else if "%eha%" == "32-static" (
+  set archIsValid=1
+  set archIs64bit=0
+) else if "%eha%" == "64" (
+  set archIsValid=1
+  set archIs64bit=1
+) else if "%eha%" == "64-debug" (
+  set archIsValid=1
+  set archIs64bit=1
+) else if "%eha%" == "64-static" (
+  set archIsValid=1
+  set archIs64bit=1
+) else if not defined eha (
+  echo   Using default: 64-static
+  set eha=64-static
+  set archIs64bit=1
+  goto SET_ENV
+)
+
+if defined archIsValid (
+  echo    You chose %eha%
+) else (
+  echo    Invalid arch!
+  goto GET_ARCH
+)
+
+:SET_ENV
+
+if %archIs64bit% EQU 1 (
+  set filename=%~dp0VS%vs%\windows-x%eha%.bat
+) else (
+  set filename=%~dp0VS%vs%\win32-x%eha:32=86%.bat
+)
+
+echo Sourcing %filename%
+echo launch dir: "%~dp0"
+echo current dir: "%CD%"
+
+cmd.exe /K %filename%
+
+echo pause
+pause
+
+endlocal 
+
+rem cmd.exe /K D:\epics\base-3.14.12.5\startup\windows-x64.bat
